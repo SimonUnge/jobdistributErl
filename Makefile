3b047edc21c9e1c71606075b45dd8012fdbd6c6a
Simon Unge <simonunge@gmail.com>
2013-01-20 20:54:30 +0100

    second try

---
+++ Makefile
@@ -0,0 +1,19 @@
+REBAR=`which rebar || ./rebar`
+
+all: deps compile
+
+deps:
+	@$(REBAR) get-deps
+compile:
+	@$(REBAR) compile
+tests: eunit ct
+
+eunit:
+	@$(REBAR) skip_deps=true eunit
+ct:
+	@$(REBAR) ct
+clean:
+	@$(REBAR) clean
+	@rm -r ebin/ || true
+	@rm -r logs/ || true
+	@rm -r test/*.beam || true
---
+++ test/jd_SUITE.erl
@@ -0,0 +1,24 @@
+-module(jd_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-export([all/0, groups/0, init_per_group/2, end_per_group/2]).
+-export([send_job_recv_result/1]).
+
+all() ->
+    [{group, jd}].
+
+groups() ->
+    [{jd, [], [send_job_recv_result]}].
+
+init_per_group(jd, Config) ->
+    ok = application:start(jobdistributerl),
+    Config;
+init_per_group(_, Config) ->
+    Config.
+
+end_per_group(jd, Config) ->
+    ok = application:stop(jobdistributerl);
+end_per_group(_, _Config) ->
+    ok.
+
+send_job_recv_result(_Config) ->
+    1 = 1.
